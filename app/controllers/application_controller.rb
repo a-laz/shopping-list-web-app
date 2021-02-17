1bd233893acf77e38fbc310afa1ccc838fa6ff03
a-laz <mortaleusmarketing@gmail.com>
2021-02-17 14:17:13 -0500

    application controller and helper methods

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < Sinatra::Base
+    configure do
+        set :views, proc { File.join(root, "../views/") }
+        enable :sessions
+        set :session_secret, "shopping_web_app"
+    end
+
+    get '/' do
+        erb :"application/index"
+    end
+
+    helpers do
+        def logged_in?
+            !!current_user
+        end
+
+        def current_user
+            @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+        end
+    end
+end
